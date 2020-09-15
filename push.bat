6470797b4694a2983880e480e335ee93874b0356
zhanglei0036 <zhanglei_0036@163.com>
2020-09-15 14:42:21 +0800

    update

--- push.bat
+++ push.bat
@@ -1,10 +1,4 @@
 @echo off
-cd /FirstGit
-git pull
-git checkout -b branch1 origin/branch1
-git add .
-git commit -m "commit message"
-git pull
 git add .
 git commit -m "update"
 git push origin branch1
